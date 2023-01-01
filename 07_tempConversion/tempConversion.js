2cf6431d4705ccd52b400882acbe6bc4e5d218fb
oliveryjy <yjyoliver@hotmail.com>
2023-01-01 20:27:05 +0800

    completed 07_tempConversion and added solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  converted = (fahrenheit - 32) * (5/9);
+  
+  // https://stackoverflow.com/questions/7342957/how-do-you-round-to-1-decimal-place-in-javascript
+  // Rounding to 1 d.p.
+  return Math.round(converted * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreeCelsius) {
+  converted = (degreeCelsius * 9/5) + 32;
+  return Math.round(converted * 10) / 10;
 };
 
 // Do not edit below this line
