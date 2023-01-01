5b784f5e54999014ebee3ecc48e98992efd389d3
oliveryjy <yjyoliver@hotmail.com>
2023-01-01 15:29:56 +0800

    completed 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(stringInput, number) {
+    // Declare final string variable
+    // Note: Need to set variable to empty string, otherwise 'undefined' will be returned.
+    let finalString = '';
+    
+    // Return error if number is negative
+    if (number < 0) {
+        return 'ERROR';
+    }
+    
+    // Concat appropriate number of stringInput into finalString.
+    for (let i = 0; i < number; i++) {
+        finalString += stringInput;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
