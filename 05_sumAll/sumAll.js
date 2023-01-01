db23d29ba7976d815823f7d97a94360323af7e19
oliveryjy <yjyoliver@hotmail.com>
2023-01-01 16:59:33 +0800

    completed 05_sumAll and added TOP solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
+const sumAll = function(numberOne, numberTwo) {
+    // Error checking for negative numbers
+    if (numberOne < 0 || numberTwo < 0) {
+        return 'ERROR';
+    }
 
+    // Error checking for non-number arguments (string and array)
+    if (typeof numberOne != "number" || typeof numberTwo != "number") {
+        return 'ERROR';
+    }
+
+    // Work with smaller number as first argument.
+    if (numberOne < numberTwo) {
+        let sum = numberOne;
+        // Starting number is numberOne + 1, then add all the way to numberTwo.
+        for (let i = numberOne + 1; i <= numberTwo; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    // Work with larger number first argument. (essentially reversed)
+    if (numberOne > numberTwo) {
+        let sum = numberTwo;
+        // Starting number is numberOne + 1, then add all the way to numberTwo.
+        for (let i = numberTwo + 1; i <= numberOne; i++) {
+            sum += i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
