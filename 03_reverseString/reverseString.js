1842be0a7228b8cadbd9469297e3a665bebce7a6
oliveryjy <yjyoliver@hotmail.com>
2023-01-01 15:43:04 +0800

    completed 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(stringInput) {
+    return stringInput.split('').reverse().join('');
 };
+
+// https://www.freecodecamp.org/news/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb/
+
 
 // Do not edit below this line
 module.exports = reverseString;
