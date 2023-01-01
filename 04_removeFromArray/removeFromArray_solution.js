728586d92b4ceff1b81ad937ae582072230bd20e
oliveryjy <yjyoliver@hotmail.com>
2023-01-01 16:17:17 +0800

    edited the solution to make it clear

--- 04_removeFromArray/removeFromArray_solution.js
+++ 04_removeFromArray/removeFromArray_solution.js
@@ -24,7 +24,7 @@ const removeFromArray = function (...args) {
   
   // var removeFromArray = function(...args) {
   //   const array = args[0]
-  //   return array.filter(val => !args.includes(val))
+  //   return array.filter((val) => !args.includes(val))
   // }
   //
   
