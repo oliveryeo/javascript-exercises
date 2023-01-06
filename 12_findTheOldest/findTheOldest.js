246861e55f35ca3f46aaccdb68553565355200a9
oliveryjy <yjyoliver@hotmail.com>
2023-01-06 22:20:12 +0800

    Completed 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.sort((a, b) => {
+        if (!a.yearOfDeath) {
+            aAge = 2023 - a.yearOfBirth;
+        } else {
+            aAge = a.yearOfDeath - a.yearOfBirth;
+        }
+        
+        if (!b.yearOfDeath) {
+            bAge = 2023 - b.yearOfBirth;
+        } else {
+            bAge = b.yearOfDeath - b.yearOfBirth;
+        }
+        
+        if (aAge > bAge) {
+            return -1;
+        } else {
+            return 1;
+        }
+    });
 
+    return people[0];
 };
 
 // Do not edit below this line
